34f060742b0712730067e98a7a3ce346236f112c
math <ilya.chervak.math@gmail.com>
2022-07-12 10:13:56 +0300

    correct adaptive, delete headerState

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,6 +4,7 @@ import Logo from "../Logo/Logo";
 import {RouteNames} from "../../router/route";
 import HeaderBtn from "./headerBtn";
 import {useAppSelector} from "../../hooks/redux";
+import {useLocation} from "react-router-dom";
 
 const Header:FC = () => {
     const profImg = require("../../media/prof.png")
@@ -12,13 +13,14 @@ const Header:FC = () => {
     const articleImg = require("../../media/article.png")
 
     const {isAuth} = useAppSelector(state => state.auth)
-    const {isActive} = useAppSelector(state => state.header)
+
+    const location = useLocation()
 
     return (
 
         <div className={classes.header}>
             <Logo/>
-            <div className={`${classes.headerPanelBtn} ${isActive && classes.active}`}>
+            <div className={`${classes.headerPanelBtn} ${location.pathname === "/" && classes.active}`}>
                 <HeaderBtn imgPath={viewImg} name="взгляд" destination={RouteNames.VIEW}/>
                 <HeaderBtn imgPath={articleImg} name="кампус" destination={RouteNames.CAMPUS}/>
                 <HeaderBtn imgPath={profImg} name="лица" destination={RouteNames.FACES}/>
--- src/components/header/headerBtn.tsx
+++ src/components/header/headerBtn.tsx
@@ -2,9 +2,7 @@ import React, {FC} from 'react';
 import classes from "./header.module.css";
 import MyButton from "../MyButton/MyButton";
 import {RouteNames} from "../../router/route";
-import {useNavigate} from "react-router-dom";
-import {useAppDispatch, useAppSelector} from "../../hooks/redux";
-import {changeHeaderAction} from "../../store/reducers/header/headerReducer";
+import {useLocation, useNavigate} from "react-router-dom";
 import "./header.module.css";
 
 interface HeaderBtnProps {
@@ -14,16 +12,12 @@ interface HeaderBtnProps {
 }
 
 const HeaderBtn:FC<HeaderBtnProps> = ({imgPath, destination, name}) => {
-    const {isActive} = useAppSelector(state => state.header)
-    const dispatch = useAppDispatch()
     const navigate = useNavigate()
-    const navigateService = () => {
-        navigate(destination)
-        dispatch(changeHeaderAction(false))
-    }
+    const navigateService = () => {navigate(destination)}
+    const location = useLocation()
     return (
         <div className={classes.headerBtn}>
-            <img className={`${classes.headerImg} ${isActive && classes.active}`} src={imgPath} alt="view"></img>
+            <img className={`${classes.headerImg} ${location.pathname === "/" && classes.active}`} src={imgPath} alt="view"></img>
             <MyButton onClick={navigateService}>{name}</MyButton>
         </div>
     );
